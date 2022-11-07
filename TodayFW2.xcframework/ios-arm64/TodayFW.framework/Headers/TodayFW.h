52747fb27880f3fd22107724e34ee66ea8f69f5a
Manohar1990 <116875575+Manohar1990@users.noreply.github.com>
2022-11-07 14:59:21 +0530

    Add files via upload

---
+++ TodayFW2.xcframework/ios-arm64/TodayFW.framework/Headers/TodayFW.h
@@ -0,0 +1,18 @@
+//
+//  TodayFW.h
+//  TodayFW
+//
+//  Created by Manohar Pitla on 07/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TodayFW.
+FOUNDATION_EXPORT double TodayFWVersionNumber;
+
+//! Project version string for TodayFW.
+FOUNDATION_EXPORT const unsigned char TodayFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TodayFW/PublicHeader.h>
+
+
